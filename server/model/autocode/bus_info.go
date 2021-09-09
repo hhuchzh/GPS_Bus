c98afce3b1e138639351fabe3ac9e7d3f7a29fa0
j8906.dong <j8906.dong@samsung.com>
2021-09-09 11:21:16 +0800

    add association between gps & bus

--- server/model/autocode/bus_info.go
+++ server/model/autocode/bus_info.go
@@ -3,24 +3,28 @@ package autocode
 
 import (
 	"github.com/flipped-aurora/gin-vue-admin/server/global"
+	"gorm.io/gorm"
 )
 
 // BusInfo 结构体
 // 如果含有time.Time 请自行import time包
 type BusInfo struct {
-      global.GVA_MODEL
-      BusUserPhone  string `json:"busUserPhone" form:"busUserPhone" gorm:"column:bus_user_phone;comment:车主电话;type:varchar(100);"`
-      BusUserName  string `json:"busUserName" form:"busUserName" gorm:"column:bus_user_name;comment:车主姓名;type:varchar(100);"`
-      BusPlate  string `json:"busPlate" form:"busPlate" gorm:"column:bus_plate;comment:车牌;type:varchar(50);"`
-      SeatNumber  *int `json:"seatNumber" form:"seatNumber" gorm:"column:seat_number;comment:座位数;type:int"`
-      CreateUserId  *int `json:"createUserId" form:"createUserId" gorm:"column:create_user_id;comment:创建用户id;type:bigint"`
-      UpdateUserId  *int `json:"updateUserId" form:"updateUserId" gorm:"column:update_user_id;comment:更新用户id;type:bigint"`
-      CompanyId  *int `json:"companyId" form:"companyId" gorm:"column:company_id;comment:公司id;type:bigint"`
+	global.GVA_MODEL
+	BusUserPhone string    `json:"busUserPhone" form:"busUserPhone" gorm:"column:bus_user_phone;comment:车主电话;type:varchar(100);"`
+	BusUserName  string    `json:"busUserName" form:"busUserName" gorm:"column:bus_user_name;comment:车主姓名;type:varchar(100);"`
+	BusPlate     string    `json:"busPlate" form:"busPlate" gorm:"column:bus_plate;comment:车牌;type:varchar(50);"`
+	SeatNumber   *int      `json:"seatNumber" form:"seatNumber" gorm:"column:seat_number;comment:座位数;type:int"`
+	CreateUserId *int      `json:"createUserId" form:"createUserId" gorm:"column:create_user_id;comment:创建用户id;type:bigint"`
+	UpdateUserId *int      `json:"updateUserId" form:"updateUserId" gorm:"column:update_user_id;comment:更新用户id;type:bigint"`
+	CompanyId    *int      `json:"companyId" form:"companyId" gorm:"column:company_id;comment:公司id;type:bigint"`
+	GpsInfos     []GpsInfo `gorm:"foreignKey:BusId"`
 }
-
 
 // TableName BusInfo 表名
 func (BusInfo) TableName() string {
-  return "bus_info"
+	return "bus_info"
 }
 
+func (bus *BusInfo) AfterDelete(tx *gorm.DB) (err error) {
+	return tx.Model(&GpsInfo{}).Where("bus_id = ?", bus.ID).Update("BusId", nil).Error
+}
--- server/service/autocode/bus_info.go
+++ server/service/autocode/bus_info.go
@@ -3,8 +3,8 @@ package autocode
 import (
 	"github.com/flipped-aurora/gin-vue-admin/server/global"
 	"github.com/flipped-aurora/gin-vue-admin/server/model/autocode"
+	autoCodeReq "github.com/flipped-aurora/gin-vue-admin/server/model/autocode/request"
 	"github.com/flipped-aurora/gin-vue-admin/server/model/common/request"
-    autoCodeReq "github.com/flipped-aurora/gin-vue-admin/server/model/autocode/request"
 )
 
 type BusInfoService struct {
@@ -19,54 +19,54 @@ func (busInfoService *BusInfoService) CreateBusInfo(busInfo autocode.BusInfo) (e
 
 // DeleteBusInfo 删除BusInfo记录
 // Author [piexlmax](https://github.com/piexlmax)
-func (busInfoService *BusInfoService)DeleteBusInfo(busInfo autocode.BusInfo) (err error) {
+func (busInfoService *BusInfoService) DeleteBusInfo(busInfo autocode.BusInfo) (err error) {
 	err = global.GVA_DB.Delete(&busInfo).Error
 	return err
 }
 
 // DeleteBusInfoByIds 批量删除BusInfo记录
 // Author [piexlmax](https://github.com/piexlmax)
-func (busInfoService *BusInfoService)DeleteBusInfoByIds(ids request.IdsReq) (err error) {
-	err = global.GVA_DB.Delete(&[]autocode.BusInfo{},"id in ?",ids.Ids).Error
+func (busInfoService *BusInfoService) DeleteBusInfoByIds(ids request.IdsReq) (err error) {
+	err = global.GVA_DB.Delete(&[]autocode.BusInfo{}, "id in ?", ids.Ids).Error
 	return err
 }
 
 // UpdateBusInfo 更新BusInfo记录
 // Author [piexlmax](https://github.com/piexlmax)
-func (busInfoService *BusInfoService)UpdateBusInfo(busInfo autocode.BusInfo) (err error) {
+func (busInfoService *BusInfoService) UpdateBusInfo(busInfo autocode.BusInfo) (err error) {
 	err = global.GVA_DB.Save(&busInfo).Error
 	return err
 }
 
 // GetBusInfo 根据id获取BusInfo记录
 // Author [piexlmax](https://github.com/piexlmax)
-func (busInfoService *BusInfoService)GetBusInfo(id uint) (err error, busInfo autocode.BusInfo) {
-	err = global.GVA_DB.Where("id = ?", id).First(&busInfo).Error
+func (busInfoService *BusInfoService) GetBusInfo(id uint) (err error, busInfo autocode.BusInfo) {
+	err = global.GVA_DB.Where("id = ?", id).Preload("GpsInfos").First(&busInfo).Error
 	return
 }
 
 // GetBusInfoInfoList 分页获取BusInfo记录
 // Author [piexlmax](https://github.com/piexlmax)
-func (busInfoService *BusInfoService)GetBusInfoInfoList(info autoCodeReq.BusInfoSearch) (err error, list interface{}, total int64) {
+func (busInfoService *BusInfoService) GetBusInfoInfoList(info autoCodeReq.BusInfoSearch) (err error, list interface{}, total int64) {
 	limit := info.PageSize
 	offset := info.PageSize * (info.Page - 1)
-    // 创建db
+	// 创建db
 	db := global.GVA_DB.Model(&autocode.BusInfo{})
-    var busInfos []autocode.BusInfo
-    // 如果有条件搜索 下方会自动创建搜索语句
-    if info.BusUserPhone != "" {
-        db = db.Where("`bus_user_phone` = ?",info.BusUserPhone)
-    }
-    if info.BusUserName != "" {
-        db = db.Where("`bus_user_name` = ?",info.BusUserName)
-    }
-    if info.BusPlate != "" {
-        db = db.Where("`bus_plate` = ?",info.BusPlate)
-    }
-    if info.CompanyId != nil {
-        db = db.Where("`company_id` = ?",info.CompanyId)
-    }
+	var busInfos []autocode.BusInfo
+	// 如果有条件搜索 下方会自动创建搜索语句
+	if info.BusUserPhone != "" {
+		db = db.Where("`bus_user_phone` = ?", info.BusUserPhone)
+	}
+	if info.BusUserName != "" {
+		db = db.Where("`bus_user_name` = ?", info.BusUserName)
+	}
+	if info.BusPlate != "" {
+		db = db.Where("`bus_plate` = ?", info.BusPlate)
+	}
+	if info.CompanyId != nil {
+		db = db.Where("`company_id` = ?", info.CompanyId)
+	}
 	err = db.Count(&total).Error
-	err = db.Limit(limit).Offset(offset).Find(&busInfos).Error
+	err = db.Limit(limit).Offset(offset).Preload("GpsInfos").Find(&busInfos).Error
 	return err, busInfos, total
 }
