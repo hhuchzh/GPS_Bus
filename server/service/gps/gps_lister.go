f67e713bb9bef2e62a33560a3c73a1fd059d0837
chenzhen <zhen1.chen@samsung.com>
2021-12-30 15:20:10 +0800

    fix the format of log
    Signed-off-by: chenzhen <zhen1.chen@samsung.com>

--- server/service/gps/gps_lister.go
+++ server/service/gps/gps_lister.go
@@ -1,11 +1,11 @@
 package gps
 
 import (
-    "fmt"
+	"github.com/robfig/cron/v3"
+	"go.uber.org/zap"
 
-    "github.com/robfig/cron/v3"
-
-    "github.com/flipped-aurora/gin-vue-admin/server/service/gps/openapi"
+	"github.com/flipped-aurora/gin-vue-admin/server/global"
+	"github.com/flipped-aurora/gin-vue-admin/server/service/gps/openapi"
 )
 
 const (
@@ -13,76 +13,76 @@ const (
 )
 
 var (
-    GpsLister = NewGPSLister()
+	GpsLister = NewGPSLister()
 )
 
 func init() {
-    GpsLister.Start()
+	GpsLister.Start()
 }
 
 type GPSLister struct {
-    client *openapi.Client
-    lc *locationCache
-    stopCh chan struct{}
-    cron *cron.Cron
-    backend Backend
+	client  *openapi.Client
+	lc      *locationCache
+	stopCh  chan struct{}
+	cron    *cron.Cron
+	backend Backend
 }
 
 func NewGPSLister() *GPSLister {
-    return &GPSLister{
-        client: openapi.NewClient(),
-        lc: NewLocationCache(),
-        stopCh: make(chan struct{}),
-        backend: NewBackend(),
-    }
+	return &GPSLister{
+		client:  openapi.NewClient(),
+		lc:      NewLocationCache(),
+		stopCh:  make(chan struct{}),
+		backend: NewBackend(),
+	}
 }
 
 func (g *GPSLister) GetLocation(imei string) (openapi.Location, error) {
-    return g.lc.get(imei)
+	return g.lc.get(imei)
 }
 
 func (g *GPSLister) ListLocation() ([]openapi.Location, error) {
-    return g.lc.list()
+	return g.lc.list()
 }
 
 func (g *GPSLister) ListDevice() ([]openapi.Device, error) {
-    device, err := g.client.ListDevice()
-    if err != nil {
-        return nil, err
-    }
+	device, err := g.client.ListDevice()
+	if err != nil {
+		return nil, err
+	}
 
-    dList := make([]openapi.Device, 0, len(device))
-    for _, d := range device {
-        dList = append(dList, *d)
-    }
-    return dList, nil
+	dList := make([]openapi.Device, 0, len(device))
+	for _, d := range device {
+		dList = append(dList, *d)
+	}
+	return dList, nil
 }
 
 func (g *GPSLister) ListTrack(imei, beginTime, endTime string) (interface{}, error) {
-    return g.backend.ListTrack(imei, beginTime, endTime)
+	return g.backend.ListTrack(imei, beginTime, endTime)
 }
 
 func (g *GPSLister) Start() {
-    fmt.Println("****** GPSLister Start")
-    g.cron = cron.New(cron.WithSeconds())
-    _, err := g.cron.AddFunc(cronJobSpec, func() {
-        location, err := g.client.ListLocation()
-        if err != nil {
-            fmt.Println("GPSLister client.ListLocation failed: ", err)
-        } else {
-            diff := g.lc.replace(location)
-            g.backend.Save(diff)
-        }
-    })
-    if err != nil {
-        fmt.Println("GPSLister Start crontab failed: ", err)
-        return
-    }
-    g.cron.Start()
-    g.client.Start()
+	global.GVA_LOG.Info("****** GPSLister Start")
+	g.cron = cron.New(cron.WithSeconds())
+	_, err := g.cron.AddFunc(cronJobSpec, func() {
+		location, err := g.client.ListLocation()
+		if err != nil {
+			global.GVA_LOG.Error("GPSLister client.ListLocation failed: ", zap.Error(err))
+		} else {
+			diff := g.lc.replace(location)
+			g.backend.Save(diff)
+		}
+	})
+	if err != nil {
+		global.GVA_LOG.Error("GPSLister Start crontab failed: ", zap.Error(err))
+		return
+	}
+	g.cron.Start()
+	g.client.Start()
 }
 
 func (g *GPSLister) Stop() {
-    g.client.Stop()
-    g.cron.Stop()
+	g.client.Stop()
+	g.cron.Stop()
 }
--- server/service/gps/openapi/token.go
+++ server/service/gps/openapi/token.go
@@ -1,113 +1,115 @@
 package openapi
 
 import (
-    "time"
-    "sync"
-    "fmt"
-    "io/ioutil"
-    "encoding/json"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"sync"
+	"time"
+
+	"github.com/flipped-aurora/gin-vue-admin/server/global"
 )
 
-type getFunc func () (*AccessToken, error)
-type refreshFunc func () (*AccessToken, error)
+type getFunc func() (*AccessToken, error)
+type refreshFunc func() (*AccessToken, error)
 
 type token struct {
-    lastToken *AccessToken
-    mu sync.RWMutex
-    get getFunc
-    refresh refreshFunc
-    needUpdate chan struct {}
+	lastToken  *AccessToken
+	mu         sync.RWMutex
+	get        getFunc
+	refresh    refreshFunc
+	needUpdate chan struct{}
 }
 
 func (t *token) start(get getFunc, refresh refreshFunc) {
-    t.get = get
-    t.refresh = refresh
-    t.needUpdate = make(chan struct{})
-    err := t.loadToken()
-    if err != nil {
-        t.update()
-        t.saveToken()
-    }
+	t.get = get
+	t.refresh = refresh
+	t.needUpdate = make(chan struct{})
+	err := t.loadToken()
+	if err != nil {
+		t.update()
+		t.saveToken()
+	}
 
-    go func () {
-        for {
-            select {
-            case <-t.needUpdate:
-                    fmt.Println("--> token need update")
-                    t.update()
-                    t.saveToken()
-            }
-        }
-    }()
+	go func() {
+		for {
+			select {
+			case <-t.needUpdate:
+				global.GVA_LOG.Info("--> token need update")
+				t.update()
+				t.saveToken()
+			}
+		}
+	}()
 }
 
 func (t *token) stop() {
 }
 
 func (t *token) updateToken() {
-    t.needUpdate <- struct{}{}
+	t.needUpdate <- struct{}{}
 }
 
 func (t *token) update() {
-    t.mu.Lock()
-    defer t.mu.Unlock()
-    token, err := t.get()
-    if err != nil {
-       return
-    }
-    t.lastToken = token
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	token, err := t.get()
+	if err != nil {
+		return
+	}
+	t.lastToken = token
 }
 
 func (t *token) forceUpdate() {
-    t.mu.Lock()
-    defer t.mu.Unlock()
-    newToken, err := t.refresh()
-    if err != nil {
-        return
-    }
-    t.lastToken = newToken
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	newToken, err := t.refresh()
+	if err != nil {
+		return
+	}
+	t.lastToken = newToken
 }
 
 func (t *token) loadToken() error {
-    cont, err := ioutil.ReadFile("openapi_token.json")
-    if err != nil {
-        return fmt.Errorf("ReadFile openapi_token.json error")
-    }
-    t.lastToken = &AccessToken{}
-    return json.Unmarshal(cont, t.lastToken)
+	cont, err := ioutil.ReadFile("openapi_token.json")
+	if err != nil {
+		return fmt.Errorf("ReadFile openapi_token.json error")
+	}
+	t.lastToken = &AccessToken{}
+	return json.Unmarshal(cont, t.lastToken)
 }
 
 func (t *token) saveToken() {
-    if t.lastToken == nil {
-        return
-    }
-    b, err := json.MarshalIndent(t.lastToken, "", "\t")
-    if err != nil {
-        return
-    }
-    _ = ioutil.WriteFile("openapi_token.json", b, 0666)
+	if t.lastToken == nil {
+		return
+	}
+	b, err := json.MarshalIndent(t.lastToken, "", "\t")
+	if err != nil {
+		return
+	}
+	_ = ioutil.WriteFile("openapi_token.json", b, 0666)
 }
 
 func (t *token) getToken() (string, error) {
-    t.mu.RLock()
-    defer t.mu.RUnlock()
-    if t.lastToken == nil {
-        return "", fmt.Errorf("access token null")
-    }
-    token := t.lastToken.AccessToken
-    return token, nil
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+	if t.lastToken == nil {
+		return "", fmt.Errorf("access token null")
+	}
+	token := t.lastToken.AccessToken
+	return token, nil
 }
 
 func timeDelay(v string) (int64, error) {
-    location, _ := time.LoadLocation("Asia/Shanghai")
-    t, err := time.ParseInLocation("2006-01-02 15:04:05", v, location)
-    if err != nil {
-        return 0, fmt.Errorf("ParseInLocation error")
-    }
-    delay := time.Now().Unix() - t.Unix()
-    delay = tokenExpiresIn - delay
-    if delay > 60 {
-        delay -= 60
-    }
-    return delay, nil
+	location, _ := time.LoadLocation("Asia/Shanghai")
+	t, err := time.ParseInLocation("2006-01-02 15:04:05", v, location)
+	if err != nil {
+		return 0, fmt.Errorf("ParseInLocation error")
+	}
+	delay := time.Now().Unix() - t.Unix()
+	delay = tokenExpiresIn - delay
+	if delay > 60 {
+		delay -= 60
+	}
+	return delay, nil
 }
